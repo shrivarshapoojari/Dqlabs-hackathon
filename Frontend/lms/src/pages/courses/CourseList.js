ac59ef1166ecd903db847012936ad77e01511893
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-08-04 18:41:43 +0530

    changes to course

--- Frontend/lms/src/pages/courses/CourseList.js
+++ Frontend/lms/src/pages/courses/CourseList.js
@@ -25,13 +25,13 @@ const CourseList = () => {
  
   return (
      <Second>
-        <div className='min-h-[90vh] pt-12 pl-20 flex flex-col gap-10 text-white'>
+        <div className=' min-h-[90vh] pt-12 pl-20 flex flex-col  gap-10 text-white min-w-[414px]'>
                 <h1 className='text-center text-4xl font-semibold mb-5'> 
                     Explore courses made by {" "}
                     <span className='font-bold text-purple-500'>Industry Experts</span>
                     
                  </h1>
-                 <div className='mb-10 flex flex-wrap gap-14'>
+                 <div className='mb-10 flex sm:flex-row flex-wrap sm:gap-14 sm:justify-evenly flex-col justify-center gap-8 '>
                        {courseList?.map((element)=>{
                          return <CourseCard key={element._id} data={element}/>
                            })} 
