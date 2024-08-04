80432342ad606b7eef0b2c875c2bfb5910dc86fa
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-08-04 18:39:14 +0530

    ui bug fix

--- Frontend/lms/src/components/Home.js
+++ Frontend/lms/src/components/Home.js
@@ -1,3 +1,6 @@
+ 
+
+
 import React from "react";
  
 import { Link } from "react-router-dom";
@@ -6,12 +9,12 @@ import Second from "../layouts/Second";
 const Home = () => {
   return (
     <Second>
-      <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16  h-[90vh]">
-        <div className="w-1/2 space-y-6">
+      <div className="pt-10 text-white flex md:flex-row items-center justify-center gap-10 mx-16  h-[90vh] flex-col">
+        <div className="md:w-1/2 space-y-6">
             <h1 className="text-5xl font-semibold">
                 Find Out best <span className="text-purple-500 font-bold"> Online Courses </span>
             </h1>
-            <p className="text-xl text-gray-200">
+            <p className="md :text-xl  text-gray-200 text-lg">
             Gramin Shakthi is a low-cost platform designed to equip rural populations with relevant life and career skills, fostering financial independence and sustainable growth. Through affordable and interactive training, we bridge the skills gap and enhances employability.
             </p>
             <div className="space-x-6">
